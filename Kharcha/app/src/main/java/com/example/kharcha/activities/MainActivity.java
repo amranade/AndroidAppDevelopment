bdb5d086e35347745ee35ee352b794ab5d13ee43
amranade <53419383+amranade@users.noreply.github.com>
2020-04-30 00:23:33 +0530

    Adding test login info (#1)
    Co-authored-by: ameya <ameyaar@gmail.com>

--- Kharcha/app/src/main/java/com/example/kharcha/activities/MainActivity.java
+++ Kharcha/app/src/main/java/com/example/kharcha/activities/MainActivity.java
@@ -1,8 +1,12 @@
-package com.example.kharcha;
+package com.example.kharcha.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.kharcha.R;
+import com.example.kharcha.data.LoginInformation;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView userNameTextView = findViewById(R.id.user_name);
+        userNameTextView.setText(LoginInformation.getName());
     }
 }
