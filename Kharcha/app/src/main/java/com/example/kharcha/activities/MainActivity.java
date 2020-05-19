509d92ca509eb3e609f7e5dc4485df165c8b3ae5
ameya <ameyaar@gmail.com>
2020-05-19 20:22:16 +0530

    Adding note user

--- Kharcha/app/src/main/java/com/example/kharcha/activities/MainActivity.java
+++ Kharcha/app/src/main/java/com/example/kharcha/activities/MainActivity.java
@@ -1,20 +1,133 @@
 package com.example.kharcha.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.kharcha.R;
-import com.example.kharcha.data.LoginInformation;
+import com.example.kharcha.models.User;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Arrays;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int RC_SIGN_IN = 123;
+
+    private DatabaseReference mDatabase;
+
+    TextView userNameView;
+    Button signInButton;
+    Button signOutButton;
+    Button noteUserButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView userNameTextView = findViewById(R.id.user_name);
-        userNameTextView.setText(LoginInformation.getName());
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        userNameView = findViewById(R.id.user_name);
+        signInButton = findViewById(R.id.sign_in);
+        signOutButton = findViewById(R.id.sign_out);
+        noteUserButton = findViewById(R.id.note_users);
+        updateUI();
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startSignInActivity();
+            }
+        });
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AuthUI.getInstance()
+                        .signOut(MainActivity.this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            public void onComplete(@NonNull Task<Void> task) {
+                                updateUI();
+                            }
+                        });
+            }
+        });
+        noteUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Here, user is available
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                User userModel = new User(user.getDisplayName());
+                mDatabase.child("users").child(user.getUid()).setValue(userModel);
+            }
+        });
+    }
+
+    private void updateUI() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user == null) {
+            userNameView.setVisibility(View.GONE);
+            signInButton.setVisibility(View.VISIBLE);
+            signOutButton.setVisibility(View.GONE);
+        } else {
+            userNameView.setVisibility(View.VISIBLE);
+            userNameView.setText(user.getDisplayName());
+            signInButton.setVisibility(View.GONE);
+            signOutButton.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void startSignInActivity() {
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.PhoneBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build());
+
+// Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateUI();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                updateUI();
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
     }
 }
